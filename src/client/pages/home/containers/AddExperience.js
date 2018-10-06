34847032ffa48f3d3d68dcf77c12e295abbf2a12
david castro <davidcastro853@gmail.com>
2018-10-05 23:34:34 -0700

    oops

--- src/client/pages/home/containers/AddExperience.js
+++ src/client/pages/home/containers/AddExperience.js
@@ -1,6 +1,6 @@
 import React {Component} from 'react'
 import './AddExperience.css'
-import {Button} from 'react-bootstrap'
+import {Button, Modal} from 'react-bootstrap'
 /**
 class AddExperience extends React.Component {
 	constructor(props){
