9498755f18f16984526481ee5b712c6c2269f583
Ryan Stevens <stevens.ryan@gmail.com>
2018-10-06 03:46:13 -0700

    patches

--- src/scripts/dynamo-migrations/00001.00.js
+++ src/scripts/dynamo-migrations/00001.00.js
@@ -3,6 +3,18 @@ const dynamoUtils = require('dynamo-utils');
 module.exports = async function() {
 
   await dynamoUtils.dropAllTables();
+  
+  await dynamoUtils.createTable('Business', {
+    create: true, // Create table in DB, if it does not exist,
+    update: true, // Update remote indexes if they do not match local index structure
+    waitForActive: false, // Wait for table to be created before trying to use it
+    waitForActiveTimeout: 10, // wait 3 minutes for table to activate
+    streamOptions: { // sets table stream options
+      enabled: false, // sets if stream is enabled on the table
+      type: undefined // sets the stream type (NEW_IMAGE | OLD_IMAGE | NEW_AND_OLD_IMAGES | KEYS_ONLY) (https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_StreamSpecification.html#DDB-Type-StreamSpecification-StreamViewType)
+    },
+    serverSideEncryption: false // Set SSESpecification.Enabled (server-side encryption) to true or false (default: true)
+  });
 
   await dynamoUtils.createTable('User', {
     create: true, // Create table in DB, if it does not exist,
@@ -17,18 +29,6 @@ module.exports = async function() {
   });
   
   await dynamoUtils.createTable('Content', {
-    create: true, // Create table in DB, if it does not exist,
-    update: true, // Update remote indexes if they do not match local index structure
-    waitForActive: false, // Wait for table to be created before trying to use it
-    waitForActiveTimeout: 10, // wait 3 minutes for table to activate
-    streamOptions: { // sets table stream options
-      enabled: false, // sets if stream is enabled on the table
-      type: undefined // sets the stream type (NEW_IMAGE | OLD_IMAGE | NEW_AND_OLD_IMAGES | KEYS_ONLY) (https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_StreamSpecification.html#DDB-Type-StreamSpecification-StreamViewType)
-    },
-    serverSideEncryption: false // Set SSESpecification.Enabled (server-side encryption) to true or false (default: true)
-  });
-  
-  await dynamoUtils.createTable('Business', {
     create: true, // Create table in DB, if it does not exist,
     update: true, // Update remote indexes if they do not match local index structure
     waitForActive: false, // Wait for table to be created before trying to use it
--- src/scripts/dynamo-migrations/00002.00.js
+++ src/scripts/dynamo-migrations/00002.00.js
@@ -1,5 +1,6 @@
 const dynamo_utils = require('dynamo-utils');
 const node_uuid = require('node-uuid');
+const util = require('util');
 
 
 module.exports = async function() {
@@ -7,8 +8,8 @@ module.exports = async function() {
   const Content = dynamo_utils.getModel('Content');
 
   var i= 0;
-  items.forEach(item => {
-    i++;
+  for (; i<items.length; i++) {
+    let item = items[i]
     let imageOverride = {}
     if (i % 2 === 0) {
       imageOverride.fileLocation = 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350'
@@ -17,9 +18,11 @@ module.exports = async function() {
       content_uuid: node_uuid.v4()
     },item, imageOverride));
     
-    content.save();
+    const saver = util.promisify(content.save).bind(content);
+    console.log('Saving ', item);
+    await saver;
   
-  })
+  }
 
 }
 
